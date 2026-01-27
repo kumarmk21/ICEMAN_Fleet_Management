13f4f263a4cf662010a753fecef547603f9138ad
kumarmk21 <112611303+kumarmk21@users.noreply.github.com>
2026-01-27 09:11:01 +0530

    Added 20260127034044_update_standard_fuel_cost_per_km.sql

---
+++ supabase/migrations/20260127034044_update_standard_fuel_cost_per_km.sql
@@ -0,0 +1,8 @@
+/*
+  # Update Standard Fuel Cost Per KM
+
+  1. Changes to vehicles table
+    - Update standard_fuel_cost_per_km to 3 for all records
+*/
+
+UPDATE vehicles SET standard_fuel_cost_per_km = 3;
